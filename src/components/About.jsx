d02f90232d8de3634467045cdc676f7bfa3eb996
Swastik Supakar <69300297+swastik58@users.noreply.github.com>
2024-04-15 13:16:26 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,54 +1,60 @@
-import React from 'react'
-import Tilt from 'react-tilt'
-import {motion} from 'framer-motion'
-import {styles} from '../styles'
-import { services } from '../constants'
-import {fadeIn, textVariant} from '../utils/motion'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-import { Wrapper } from '../hoc'
+import { styles } from "../styles";
+import { services } from "../constants";
+import { Wrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({index, title, icon}) => {
-  return(
-    <Tilt className='xs:w-[250px] w-full'>
-      <motion.div
-        variants={fadeIn("right", "spring", 0.5*index, 0.75)}
-        className='w-full green-pink-gradient p-[1px] rounded-[20opx] shadow-card'
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
-        <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450
-          }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img src={icon} alt={title} className='w-25 h-25 object-contain rounded-md' />
-          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
-        </div>
-      </motion.div>
-    </Tilt>
-  )
-}
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText}`}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p variants={fadeIn("", "", 0.1, 1)}
-        className='mt-1 text-secondary text-[17px] max-w-3xl leading-[30px]'  
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a 2022 B.Tech graduate who embarked on a professional journey at Oracle, where I currently serve as a Full-Time Employee. 
-        My passion for technology and a perpetual hunger for learning have driven me to explore various domains. 
-        While I've been immersed in the world of ReactJS, crafting elegant and user-friendly frontend websites, my quest for knowledge knows no bounds.
-        Beyond the world of coding, my heart belongs to cricket. Although I didn't make it to the pitch professionally, the sport remains an enduring love of mine. 
-        In my leisure moments, you'll find me enjoying the thrill of a cricket match, immersing myself in the latest video games, grooving to music, or indulging in cinematic experiences 
-        with movies and series. Additionally, I'm an avid reader, with novels offering a delightful escape into different worlds.
-        My journey is one of continuous growth and exploration, both in the realm of technology and in the pursuits that ignite my passions.
-
+        I'm a 2022 B.Tech grad at Oracle, focusing on ReactJS for frontend web dev.
+        Cricket's my passion, though I play casually.
+        In my downtime, I dive into video games, music, movies, and novels.
+        Always eager to learn and grow, both in tech and beyond!
+        <br /> <br />
+        <a href="https://drive.google.com/drive/folders/1vs5TZzgFMC28uOXth4_LVBDYtYRZE0tD?usp=share_link" target="_blank">
+          <h3 className="text-[40px] hover:text-red-800">Click here to view my Résumé</h3>
+        </a>
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
@@ -56,15 +62,8 @@ const About = () => {
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-      <br /> <br />
-      <motion.div variants={textVariant()}>
-        <a href="https://drive.google.com/drive/folders/1vs5TZzgFMC28uOXth4_LVBDYtYRZE0tD?usp=share_link" target="_blank">
-          <h3 className="text-[40px] hover:text-red-800">Click here to view my Résumé</h3>
-        </a>
-      </motion.div>
-
     </>
-  )
-}
+  );
+};
 
-export default Wrapper(About, "about")
+export default Wrapper(About, "about");
